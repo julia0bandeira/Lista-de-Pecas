e8a661337ddb285d82a47830188401fec346721a
Júlia Bandeira <juliabandeira2004@gmail.com>
2022-08-14 21:12:56 -0300

    listaPecas.js

---
+++ listaPecas.js
@@ -0,0 +1,8 @@
+const listaDePecas = ('Lanterna','Roda',"Po")
+
+let pesoPeca = 100
+
+if (pesoPeca > 100) {console.log("peso ok, pode cadastrar a peça")}
+else (console.log("peça recusada"))
+
+console.log("fim do processamento")
